4c774885d203f56a82ebb47ed2071f50a67f76dd
xydpts <xydpts@gmail.com>
2025-12-04 11:10:51 +0800

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by C F on 2025/12/4.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by C F on 2025/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
